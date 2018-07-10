0a3c0f75198a44875060e566feb97c33e06c9053
Lee <lee@LeedeMacBook-Pro.local>
2018-07-10 17:31:16 +0800

    改变背景颜色

--- LYPageMenuVC/ViewController.m
+++ LYPageMenuVC/ViewController.m
@@ -24,7 +24,8 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     self.navigationItem.title = @"封装pageMenu测试";
-    self.view.backgroundColor = [UIColor colorWithRed:240.0/255.0 green:240.0/255.0 blue:240.0/255.0 alpha:1];
+//    self.view.backgroundColor = [UIColor colorWithRed:240.0/255.0 green:240.0/255.0 blue:240.0/255.0 alpha:1];
+    self.view.backgroundColor = [UIColor blueColor];
     LYBtn *btnView = [[LYBtn alloc] initWithFrame:CGRectMake(0, 64, MainWidth, 40) arr:@[@"积极",@"响应",@"党",@"和",@"中央",@"号召",@"犯我中华者",@"虽远必诛"]];
     btnView.selectedBtnTag = ^(NSInteger btnTag) {
         NSLog(@"%ld",btnTag);
